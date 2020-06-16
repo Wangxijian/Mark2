ad21a1966a8795339c12afd72a81f20e38594719
469827388 <469827388@qq.com>
2020-06-17 00:07:18 +0800

    TabLayout

--- app/src/main/java/com/stark/mark2/MainActivity.java
+++ app/src/main/java/com/stark/mark2/MainActivity.java
@@ -1,14 +1,42 @@
 package com.stark.mark2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.flyco.tablayout.CommonTabLayout;
+import com.flyco.tablayout.listener.CustomTabEntity;
+import com.stark.mark2.Bean.TabEntity;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private String[] mTitles = {"首页", "专题", "党务", "我的"};
+    private int[] mIconUnselectIds = {
+            R.mipmap.tab_home_unselect, R.mipmap.tab_speech_unselect,
+            R.mipmap.tab_contact_unselect, R.mipmap.tab_more_unselect};
+    private int[] mIconSelectIds = {
+            R.mipmap.tab_home_select, R.mipmap.tab_speech_select,
+            R.mipmap.tab_contact_select, R.mipmap.tab_more_select};
+    private ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
+    private ArrayList<Fragment> mFragments = new ArrayList<>();
+    private CommonTabLayout mTab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTab = findViewById(R.id.tab_layout);
+
+
+        mFragments.add(new HomeFragment());
+        mFragments.add(new UserFragment());
+        for (int i = 0; i < mTitles.length; i++) {
+            mTabEntities.add(new TabEntity(mTitles[i], mIconSelectIds[i], mIconUnselectIds[i]));
+        }
+        mTab.setTabData(mTabEntities,this,R.id.main,mFragments);
     }
 }
