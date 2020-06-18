3f88d87fb7c0c7cb8964a051ddcb79272672a42e
469827388 <469827388@qq.com>
2020-06-19 00:40:46 +0800

    首页

--- app/src/main/java/com/stark/mark2/HomeFragment.java
+++ app/src/main/java/com/stark/mark2/HomeFragment.java
@@ -17,16 +17,14 @@ import androidx.fragment.app.Fragment;
 public class HomeFragment extends Fragment {
 
 
-    public HomeFragment() {
-        // Required empty public constructor
-    }
 
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return view;
     }
 
     @Override
